debce3149923116dde4f2c507c592eb18c3e7f39
Henry <henry1192_2004@live.com.au>
2024-11-06 23:23:28 +1100

    added folder and connect them together in app.js

---
+++ myReactNook/src/app.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<AboutMe />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
